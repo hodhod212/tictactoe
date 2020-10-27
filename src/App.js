60dcae8bea51de7eeddaac63ce5aaaa42a707be5
Ali Ezadkhaha <hodhod212@yahoo.com>
2020-10-27 03:40:37 +0100

    added styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './styles/root.scss';
 import Board from './components/Board';
 export default () => (
-  <div>
+  <div className="app">
     <h1>Tic Tac Toe</h1>
     <Board />
   </div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import '../styles/root.scss';
 function Square({ value }) {
-  return <button type="button">{value}</button>;
+  return (
+    <button type="button" className="square">
+      {value}
+    </button>
+  );
 }
 
 export default Square;
